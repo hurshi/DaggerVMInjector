12fcb43bc1ea74af09d9ed374ce149f0083e4807
画十 <e_Gavin@163.com>
2017-10-25 10:07:27 +0800

    rename extension name

---
+++ extension/src/main/java/github/hurshi/daggervminjector/extension/DaggerVM.java
@@ -0,0 +1,8 @@
+package github.hurshi.daggervminjector.extension;
+
+/**
+ * Created by gavin on 2017/9/28.
+ */
+
+public interface DaggerVM {
+}
---
+++ extension/src/main/java/github/hurshi/daggervminjector/extension/DaggerVMKey.java
@@ -0,0 +1,20 @@
+package github.hurshi.daggervminjector.extension;
+
+import java.lang.annotation.Target;
+
+import dagger.MapKey;
+import dagger.internal.Beta;
+
+import static java.lang.annotation.ElementType.METHOD;
+
+/**
+ * Created by gavin on 2017/9/28.
+ */
+
+
+@Beta
+@MapKey
+@Target(METHOD)
+public @interface DaggerVMKey {
+    Class<? extends DaggerVM> value();
+}
